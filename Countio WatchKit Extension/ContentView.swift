1bbae171111b6999e5b9d5f5b163a9ee1d6c6605
Dhanraj Chavan <dc6707914@gmail.com>
2020-05-31 18:01:46 +0530

    Initial Project

--- Countio WatchKit Extension/ContentView.swift
+++ Countio WatchKit Extension/ContentView.swift
@@ -9,8 +9,103 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isDecrementOn = false
+    
+    @State private var currentCount = UserDefaults.standard.integer(forKey: "currentCount")
+    
     var body: some View {
-        Text("Hello, World!")
+        ScrollView {
+            VStack(alignment: .center, spacing: 10) {
+                HStack(alignment: .center, spacing: 20) {
+                    Button(action: {
+                        self.isDecrementOn.toggle()
+                    }) {
+                        Image(systemName: self.isDecrementOn ? "minus" : "plus")
+                            .font(.headline)
+                            .foregroundColor(.blue)
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    
+                    Button(action: {
+                        self.currentCount = 0
+                        UserDefaults.standard.set(0, forKey: "currentCount")
+                    }) {
+                        Image(systemName: "trash")
+                            .font(.headline)
+                            .foregroundColor(.red)
+                            .aspectRatio(contentMode: .fit)
+                    }
+                }
+                
+                Button(action: {
+                    self.performMaths(isSubtract: self.isDecrementOn, number: 1)
+                }) {
+                    Text("\(currentCount)")
+                        .font(.title)
+                }
+                
+                ScrollView(.horizontal) {
+                    HStack(spacing: 10) {
+                        Button(action: {
+                            self.performMaths(isSubtract: self.isDecrementOn, number: 1)
+                        }) {
+                            Text(self.isDecrementOn ? "-1" : "+1")
+                                .font(.headline)
+                        }
+                        
+                        Button(action: {
+                            self.performMaths(isSubtract: self.isDecrementOn, number: 2)
+                        }) {
+                            Text(self.isDecrementOn ? "-2" : "+2")
+                                .font(.headline)
+                        }
+                        
+                        Button(action: {
+                            self.performMaths(isSubtract: self.isDecrementOn, number: 3)
+                        }) {
+                            Text(self.isDecrementOn ? "-3" : "+3")
+                                .font(.headline)
+                        }
+                        
+                        Button(action: {
+                            self.performMaths(isSubtract: self.isDecrementOn, number: 4)
+                        }) {
+                            Text(self.isDecrementOn ? "-4" : "+4")
+                                .font(.headline)
+                        }
+                        
+                        Button(action: {
+                            self.performMaths(isSubtract: self.isDecrementOn, number: 5)
+                        }) {
+                            Text(self.isDecrementOn ? "-5" : "+5")
+                                .font(.headline)
+                        }
+                        
+                        Button(action: {
+                            self.performMaths(isSubtract: self.isDecrementOn, number: 10)
+                        }) {
+                            Text(self.isDecrementOn ? "-10" : "+10")
+                                .font(.headline)
+                                .frame(width: 50)
+                        }
+                    }
+                }
+            }.padding(.all)
+        }
+    }
+    
+    func performMaths(isSubtract : Bool, number : Int) {
+        var result = self.currentCount
+        if isSubtract == true {
+            result -= number
+        } else {
+            result += number
+        }
+        if result >= 0 {
+            currentCount = result
+            UserDefaults.standard.set(currentCount, forKey: "currentCount")
+        }
     }
 }
 
